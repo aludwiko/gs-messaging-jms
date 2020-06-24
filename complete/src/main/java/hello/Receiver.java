1830897fdccd480c955aad2c272f58c7ec153a3d
Andrzej Ludwikowski <andrzejludwikowski@gmail.com>
2020-06-24 11:18:20 +0200

    receiver random response time

--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -6,17 +6,20 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class Receiver {
 
     private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
 
+    private final Random random = new Random();
 
     @JmsListener(destination = "inbox", containerFactory = "myFactory")
     @SendTo("outbox")
     public String receiveMessage(String message) throws InterruptedException {
         logger.info("Received: " + message);
-        Thread.sleep(100);
+        Thread.sleep(random.nextInt(200) + 50);
         String response = message.toUpperCase();
         logger.info("Responding with: " + response);
         return response;
